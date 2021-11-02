b6883bdde38e8fa3946008b3dadd934ddb7ce7cf
Fernando Ortega <feortegas@gmail.com>
2021-11-01 23:27:49 -0500

    add event listener for Start Quiz button

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Select Start Quiz button element
+var startQuizEl = document.querySelector(".start-quiz");
+
+// Attach event listener to Start Quiz button
+startQuizEl.addEventListener("click", function() {
+    console.log("Event Listener - Click - works!");
+});
