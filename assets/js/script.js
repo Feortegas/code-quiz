6033a5025fe320932cb751885e5b4c1cc8d7b58f
Fernando Ortega <feortegas@gmail.com>
2021-11-03 23:51:14 -0500

    add question section elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 // Initialize Timer with a value
+// Initialize Score as 0
 var theQuizGame = {
     theTimer: 10,
     theScore: 0
 };
 
+// quiz questions array
 var questions = [
     {
         question: "How much is 1 + 0?",
         options: [1,2,3,4],
-        rightAnswer: 2
+        rightAnswer: 1
     },
 
     {
@@ -20,17 +22,18 @@ var questions = [
     {
         question: "How much is 1 + 2?",
         options: [1,2,3,4],
-        rightAnswer: 2
+        rightAnswer: 3
     },
 
     {
         question: "How much is 1 + 3?",
         options: [1,2,3,4],
-        rightAnswer: 2
+        rightAnswer: 4
     }
 ];
 
-
+// Select questions section element
+var questionsEL = document.getElementById("questions");
 // Select Start Quiz button element
 var startQuizEl = document.querySelector(".start-quiz");
 // Select Timer element
@@ -38,9 +41,24 @@ var timerEl = document.querySelector(".time-counter");
 
 // Attach event listener to Start Quiz button
 startQuizEl.addEventListener("click", function() {
-    console.log("Event Listener - Click - works!");
+    // hide content main section
+    hideMainContent();
+    //start time countdown
     countdown();
+    // generate questions
+    generateQuestionEls();
+    // generate buttons
+    generateAnswerEls();
 });
+
+var hideMainContent = function() {
+    var mainContentEl = document.getElementById("main-page");
+    console.log(mainContentEl);
+    mainContentEl.setAttribute("class", "hide-content");
+
+    // make questions section visible
+    questionsEL.removeAttribute("class")
+}
 
 // Timer function
 var countdown = function() {
@@ -49,8 +67,6 @@ var countdown = function() {
         if (theQuizGame.theTimer > 0) {
             timerEl.textContent = theQuizGame.theTimer;
             theQuizGame.theTimer--;
-            // generate questions
-            quizGame();
         }
         else {
             timerEl.textContent = 0;
@@ -59,14 +75,26 @@ var countdown = function() {
             return;
         }
     }, 1000);
-
-    
 };
 
-// Quiz Game function
-var quizGame = function() {
-  for (let i = 0; i < questions.length; i++) {
-      // create answer options buttons
-      var optionAnswer = document.createElement("button");
-  }
+// Generate Question Elements function
+var generateQuestionEls = function() {
+    // creante question h3 element
+    var h3El = document.createElement("h3");
+    h3El.textContent = questions[0].question;
+    questionsEL.appendChild(h3El);
+    // for (let i = 0; i < questions.length; i++) {
+    // }
 };
+
+// Generate Answer Buttons function
+var generateAnswerEls = function() {
+    for (let i = 0; i < questions[0].options.length; i++) {
+        // create answer options buttons
+        var answerBtnEl = document.createElement("button");
+        answerBtnEl.className = "btn-default";
+        answerBtnEl.textContent = questions[0].options[i];
+        questionsEL.appendChild(answerBtnEl);
+    }
+};
+
